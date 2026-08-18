e3d5aed0b5e0b5f4518809aba458196d3bc2f721
Joe Dolson <design@joedolson.com>
2026-08-18 16:45:07 -0500

    Remove hosting tag, which is no longer accurate.

--- footer.php
+++ footer.php
@@ -48,11 +48,6 @@
 			/* translators: 1: Theme name, 2: Theme author. */
 			printf( esc_html__( 'Theme: %s', 'gutenberg-starter-theme' ), '<a href="https://github.com/WordPress/gutenberg-starter-theme/">Gutenberg Starter Theme</a>' );
 		?>
-		<span class="sep"> | </span>
-		<?php
-			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Hosting: %s', 'gutenberg-starter-theme' ), '<a href="https://whodunit.fr">Whodunit</a>' );
-		?>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
